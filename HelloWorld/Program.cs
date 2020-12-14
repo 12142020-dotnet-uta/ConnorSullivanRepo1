a4d06c5ef33aac987c492e0c348349fe83af96be
Connor Sullivan <crsullivan@protonmail.com>
2020-12-14 14:35:36 -0700

    Adds for loop and len variable

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,6 +7,18 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This Batch will be Revature A-Team.");
+            Console.WriteLine("###########################\n###########################\n###########################\n###########################\n###########################");
+            Console.WriteLine("Please Enter a Word");
+            string response = Console.ReadLine();
+            int len = response.Length;
+            Console.WriteLine($"Your response was '{response}'");
+            Console.WriteLine($"Your response was '{len}' characters long");
+            for (int i = 0; i < len; i++)
+            {
+                Console.WriteLine($"{i + 1}: {response[i]}");
+            }
+            ;
         }
     }
 }
