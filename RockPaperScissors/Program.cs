23e3519b4701fcd7786fd1c0509b91299fd0c93b
Connor Sullivan <crsullivan@protonmail.com>
2020-12-15 12:50:34 -0700

    adds user rps selection and cpu rps selection

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Rock Paper Scissors!");
+            Console.WriteLine("Please Enter Your Selection:");
+            string response = Console.ReadLine();
+            string res_lower = response.ToLower();
+            Console.WriteLine($"You selected: {res_lower}");
+            string[] options = {"rock", "paper", "scissors"};
+            Random rnd = new Random();
+            int num = rnd.Next(3); 
+            string cpuresponse = options[num];
+            Console.WriteLine($"CPU selected: {cpuresponse}");
+        }
+    }
+}
