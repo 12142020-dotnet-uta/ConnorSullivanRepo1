ca16ebb305ad720c103f46bcdc88490c4732b964
Connor Sullivan <crsullivan@protonmail.com>
2020-12-16 12:52:39 -0700

    adds my personal RPS game and my group project

---
+++ RPS_Group_assn/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RPS_Group_assn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true){
+                Console.WriteLine("\nWelcome to the Rock-Paper-Scissors Game");
+                Console.WriteLine("Type \"stop\" if you want to stop.");
+                Console.WriteLine("Please choose an option by typing a number: \n\t1. Rock\n\t2. Paper\n\t3. Scissors");
+                Console.WriteLine("Type your answer here: ");
+                string userResponse = Console.ReadLine().ToLower();
+                if(userResponse=="stop"){
+                    break;
+                }
+                if(userResponse=="1"||userResponse=="2"||userResponse=="3"){
+                    Dictionary<int,string> answers = new Dictionary<int,string>();
+                    //rock beats scissors
+                    answers.Add(1,"3");
+                    //paper beats rock
+                    answers.Add(2,"1");
+                    //scissors beats paper
+                    answers.Add(3,"2");
+                    Random rand = new Random();
+                    int computerChoice = rand.Next(3)+1;
+                    string response = "The computer chose ";
+                    if(userResponse==computerChoice.ToString()){
+                        Console.WriteLine("\nResult: You tied!");
+                    }
+                    else if(answers[computerChoice]==userResponse){
+                        switch (computerChoice){
+                            case 1: response+="Rock and you picked Scissors, so you lost...";
+                            break;
+                            case 2: response+="Paper and you picked Rock, so you lost...";
+                            break;
+                            case 3: response+="Scissors and you picked Paper, so you lost...";
+                            break;
+                        }
+                        Console.WriteLine(response);
+                        Console.WriteLine("\nResult: You fool! The computer won!\n");
+                    }
+                    else{
+                        switch (computerChoice){
+                            case 1: response+="Rock and you picked Paper, so you won!";
+                            break;
+                            case 2: response+="Paper and you picked Scissors, so you won!";
+                            break;
+                            case 3: response+="Scissors and you picked Rock, so you won!";
+                            break;
+                        }
+                        Console.WriteLine(response);
+                        Console.WriteLine("\nResult: Good job! You won!\n");
+                    }
+                }
+                else{
+                    Console.WriteLine("Ummm, try again...\n\n");
+                }
+            }
+        }
+
+    }
+
+}
+
--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -6,38 +6,48 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
+            // Welcome user and initiate counter variables to track rounds and scores
             Console.WriteLine("Welcome to Rock Paper Scissors!");
-
             int counter = 0;
-
+            int userWins = 0;
+            int cpuWins = 0;
+            // Set up game loop to continually run until the user exits the program
             while (true) {
-                // show how many rounds have been played
-                counter += 1;
-                Console.WriteLine($"Round {counter}!");
                 // get user response
-                Console.WriteLine("Please Enter Your Selection:");
+                Console.WriteLine("Please Enter Your Selection (Rock, Paper, or Scissors):");
                 string userresponse = Console.ReadLine();
-                // normalize user response
+                // normalize user response and validate
                 string res_lower = userresponse.ToLower();
-                Console.WriteLine($"You selected: {res_lower}");
-                string[] options = {"rock", "paper", "scissors"};
-                // get random selection from program
-                Random rnd = new Random();
-                int num = rnd.Next(3); 
-                string cpuresponse = options[num];
-                string[] gameStatus = {res_lower, cpuresponse};
-                Console.WriteLine($"CPU selected: {cpuresponse}");
-                // check win conditions
-                if (res_lower == cpuresponse)
+                if ((res_lower == "rock" || res_lower == "paper" || res_lower == "scissors"))
                 {
-                    Console.WriteLine("Tie Game!!");
-                }
-                else if ((res_lower == "rock" & cpuresponse == "scissors") || (res_lower == "paper" & cpuresponse == "rock") || (res_lower == "scissors" & cpuresponse == "paper"))
-                {
-                    Console.WriteLine("You Win!!");
+                    counter += 1;
+                    Console.WriteLine($"You selected: {res_lower}");
+                    string[] options = {"rock", "paper", "scissors"};
+                    // get random selection from program
+                    Random rnd = new Random();
+                    int num = rnd.Next(3); 
+                    string cpuresponse = options[num];
+                    Console.WriteLine($"CPU selected: {cpuresponse}");
+                    // check win conditions and show how many rounds have been played and current score
+                    if (res_lower == cpuresponse) // tie game
+                    {
+                        Console.WriteLine($"Round {counter}: Tie Game!!\n################");
+                        Console.WriteLine($"Score: \nYou: {userWins}\nCPU: {cpuWins}");
+                    }
+                    else if ((res_lower == "rock" & cpuresponse == "scissors") || (res_lower == "paper" & cpuresponse == "rock") || (res_lower == "scissors" & cpuresponse == "paper")) // user wins
+                    {
+                        userWins += 1;
+                        Console.WriteLine($"Round {counter}: You Win!!\n################"); // user wins
+                        Console.WriteLine($"Score: \nYou: {userWins}\nCPU: {cpuWins}");
+                    }
+                    else {
+                        cpuWins += 1;
+                        Console.WriteLine($"Round {counter}: You Lose!!\n################"); // program wins
+                        Console.WriteLine($"Score: \nYou: {userWins}\nCPU: {cpuWins}");
+                    }
                 }
                 else {
-                    Console.WriteLine("You Lose!!");
+                    Console.WriteLine($"Please Enter a valid response");
                 }
             }
         }
